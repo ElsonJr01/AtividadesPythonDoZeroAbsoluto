f767a689b78266d228c2e2f9aacf3d89f5b26d7a
Elson Junior. <167557011+ElsonJr01@users.noreply.github.com>
2024-12-25 23:57:40 -0300

    Add files via upload

---
+++ cursoIniciante.py
@@ -0,0 +1,16 @@
+codigo = 10
+salario = 1500.00
+nome = 'João'
+situação = True
+
+tipo = type(salario)
+tipo2 = type(nome)
+print(salario)
+print(tipo)
+print(nome)
+print(tipo2)
+
+print("Codigo: " + str(codigo) + " nome: " + nome + " O salario e de " + str(salario))
+
+fruta = int(input("Digite o nome:   "))
+print(fruta)
